bc3479a203d6593ce6d4afa1ff646428ab42f69e
javilejoo <pra21486@uvg.edu.gt>
2022-11-30 00:47:49 -0600

    actualizacion de jupyternotebook

--- Proyecto Microprocesadores.py
+++ Proyecto Microprocesadores.py
@@ -1,24 +1,25 @@
 import serial # pip install pyserial
 
-archivo = open("Datos.txt","w")
-archivo1 = open("Datos1.txt","w")
-serialArduino = serial.Serial('COM3',9600)
+archivo = open("Datos.txt","w") #Archivo que guarda todo el contenido
+archivo1 = open("Datos1.txt","w")#archivo que guarda solo los datos de distancia
+serialArduino = serial.Serial('COM3',9600) #Puerto COM y velocidad de transmisión
 
-datos = []
-datos1 = []
+datos = []#Arreglo que guarda todo el contenido
+datos1 = []#Arreglo que guarda solo los datos de distancia
 contador = 0
 
-while (contador != 20+6):
-    distancias = serialArduino.readline().decode("ascii")
-    print(distancias)
-    datos.append(distancias)
+while (contador != 500+6):#Se recibe 500 datos y 6 de basura
+    distancias = serialArduino.readline().decode("ascii")#Se decodifica el dato
+    print(distancias)#Se imprime el dato
+    datos.append(distancias)#Se guarda el dato en el arreglo
     contador += 1
 
-archivo.writelines(datos)
-archivo.close()
+archivo.writelines(datos)#Se guarda todo el contenido en el archivo
+archivo.close()#Se cierra el archivo
 
-archivo = open("Datos.txt","r")
+archivo = open("Datos.txt","r")#Se abre el archivo en modo lectura
 for x in archivo:
+        #borramos los caracteres de basura
         xf = x.replace("La distancia es: ", "")
         cm = xf.replace(" cm", "")
         group = cm.replace("Grupo:\n", "")
@@ -26,13 +27,14 @@ for x in archivo:
         a = j.replace("Angel Perez - 21298\n","")
         b = a.replace("Bryan Espana - 21550\n", "")
         des = b.replace("Programa para calcular el tiempo que viaja un sonido y la distancia que recorre\n","")
+        #agregamos los datos a un arreglo de distancia
         datos1.append(des)
         
-archivo1.writelines(datos1) 
-archivo1.close()
-archivo.close()
+archivo1.writelines(datos1)#Se guarda solo los datos de distancia en el archivo 
+archivo1.close()#Se cierra el archivo
+archivo.close()#Se cierra el archivo
 
-exit()
+exit()#Se cierra el programa
     
 
 
